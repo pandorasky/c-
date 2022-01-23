2121782083c9270f9ceec15c674946378f4d8b46
pandorasky <93546305+pandorasky@users.noreply.github.com>
2022-01-23 17:25:07 +0530

    Add files via upload

---
+++ c++/sum_average.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+    int main()
+    {
+        float number1,number2,sum,average;
+        cout<<"Enter two numbers:";
+        
+        cin>> number1;
+        cin>> number2;
+        
+        sum = number1 + number2;
+        average = sum/2;
+        
+        cout<<"sum  = "<<sum<<"\n";
+        cout<< "average = "<< average<<"\n";
+        return 0;
+    }
